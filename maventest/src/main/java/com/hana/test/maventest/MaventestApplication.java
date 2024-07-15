c6682ff08a851ad6464bc44b568ebc2032ca3ff9
박성완 <swany0509@naver.com>
2024-07-15 23:43:40 +0900

    maven setting

---
+++ maventest/src/main/java/com/hana/test/maventest/MaventestApplication.java
@@ -0,0 +1,13 @@
+package com.hana.test.maventest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaventestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaventestApplication.class, args);
+	}
+
+}
